c4809c191eb71e153dcc7d06fef25f6ec8305a10
Dvent1123 <ventura.daniel36@gmail.com>
2022-02-22 19:52:48 -0700

    protect and unprotected routes

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,17 +1,17 @@
 
 export default function ({app, route, redirect}){
-    if (route.path !== '/auth/signin') {
-      //we are on a protected route
-      if(!app.$fire.auth.currentUser) {
-        //take them to sign in page
-        return redirect('/auth/signin')
-      }
-    } else if (route.path === '/auth/signin') {
-      if(!app.$fire.auth.currentUser) {
-        //leave them on the sign in page
-      } else {
-        return redirect('/')
-      }
+  if (route.path !== '/auth/signin' && route.path !== '/auth/signup' && route.path !== '/auth/landing') {
+    //we are on a protected route
+    if(!app.$fire.auth.currentUser) {
+      //take them to sign in page
+      return redirect('/auth/landing')
     }
   }
-  
+  if (route.path === '/auth/signin' || route.path === '/auth/landing' || route.path === '/auth/signup') {
+    if(!app.$fire.auth.currentUser) {
+      //leave them on page
+    } else {
+      return redirect('/')
+    }
+  }
+}
---
+++ pages/auth/landing.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+    <nav>
+        <NuxtLink to="/auth/signin">Signin</NuxtLink>
+        <NuxtLink to="/auth/signup">Signup</NuxtLink>
+    </nav>
+    <h1>Landing Page</h1>
+</div>
+</template>
+
+<script>
+
+export default {
+}
+</script>
