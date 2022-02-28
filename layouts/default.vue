e92738369e3bc119a8f120711a4573a6af0f35e1
Brehtyn <66739729+Brehtyn@users.noreply.github.com>
2022-02-28 07:35:05 -0800

    Navbar default.vue
    Structure, styling
    +temp project logo
    +nav menu sandwich
    +require icons for rest of nav

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,32 @@
 <template>
-    <nuxt />
+  <div>
+    <nav class="w-100 shadow-lg text-lg shadow bg-gray-100">
+      <div class="container mx-auto px-4">
+        <ul class="flex justify-around p-2 items-center">
+          <li class="hover:opacity-80">
+            <NuxtLink to="landing.vue">
+              <img class="w-16" src="../assets/logo.png" />
+            </NuxtLink>
+          </li>
+          <li class="hover:opacity-80">
+            <NuxtLink to="/"> Account </NuxtLink>
+          </li>
+          <li class="hover:opacity-80">
+            <NuxtLink to="/"> New Post </NuxtLink>
+          </li>
+          <li class="hover:opacity-80">
+            <button>Log Out</button>
+          </li>
+          <li class="hover:opacity-80">
+            <button><img class="max-w-xs" src="../assets/menu.svg" /></button>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <Nuxt />
+  </div>
 </template>
 
 <script>
-
-export default {
-    
-}
+export default {};
 </script>
